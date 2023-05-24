d0963e47e58c379d0deac31287c2e9417923f58c
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-05-24 19:26:44 +0630

    3d portfolio

---
+++ readme.md
@@ -0,0 +1,5 @@
+# npm create vite@latest ./ -- --template react
+
+## npm i -D tailwindcss
+
+## npx tailwindcss init
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>3D Web Developer Portfolio</div>
+    </>
+  );
+}
+
+export default App;
