098529231a083311691cdc540ee65b347437fc6e
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-05-26 14:41:47 +0630

    newupdate

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-const Works = () => {
-  return <div>Works</div>;
+import { styles } from "../styles";
+import { github } from "../assets";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <h2>hi</h2>
+    </motion.div>
+  );
 };
 
-export default Works;
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          variants={fadeIn("", "", 0.1, 1)}
+        ></motion.p>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
